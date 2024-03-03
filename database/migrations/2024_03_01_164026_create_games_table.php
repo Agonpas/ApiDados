9ce3b587e3b0c31f801892e9244442ad831936f4
Agonpas <agonpas@gmail.com>
2024-03-03 12:56:22 +0100

    Spatie and Passport Installed

--- database/migrations/2024_03_01_164026_create_games_table.php
+++ database/migrations/2024_03_01_164026_create_games_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('games', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('nickname')->nullable()->default('Anónimo');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('dice1');
+            $table->integer('dice2');
+            $table->boolean('won');
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('games');
     }
 };
