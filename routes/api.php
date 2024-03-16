0b0dbf6bd7af2d375fa8a427766b85b8b8596025
Agonpas <agonpas@gmail.com>
2024-03-16 18:40:48 +0100

    organized routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,13 @@ use App\Http\Controllers\PassportController;
 //Rutas accesibles sin registro:
 Route::post('/players', [PassportController::class,'register']); //registrarse
 Route::post('/login', [PassportController::class,'login']); //iniciar sesión
+
+//Rutas accesibles con registro
+Route::middleware('auth:api')->group(function () {
+    Route::put('/players/{id}', [UserController::class, 'updateName']); //modificar usuario
+    Route::post('/logout', [PassportController::class, 'logout']); //cerrar sesión 
+});
+
 // Rutas para jugadores
 Route::middleware(['auth:api', 'role:player'])->group(function () { 
     Route::post('/players/{id}/games', [GameController::class, 'createGame']); //crear juego
@@ -31,38 +38,21 @@ Route::middleware(['auth:api', 'role:admin'])->group(function () {
     Route::get('/players/ranking', [GameController::class, 'ranking']); //Ranking todos los usuarios
     Route::get('/players/ranking/winner', [GameController::class, 'winner']); //Ganadores
     Route::get('/players/ranking/loser', [GameController::class, 'loser']); //perdedores
-    Route::get('/users', [UserController::class, 'index']);//lista de usuarios
 });
+
+
+
 //Route::post('/register', [PassportController::class,'register']);
-
-//Rutas accesibles con registro
-Route::middleware('auth:api')->group(function () {
-    Route::put('/players/{id}', [UserController::class, 'updateName']); //modificar usuario
-    Route::post('/logout', [PassportController::class, 'logout']); //cerrar sesión 
-});
-
-
 //Route::post('/users/{id}/games', [GameController::class, 'createGame']); //crear juegos
 //Route::post('/players/{id}/games', [GameController::class, 'createGame']); //crear juegos
 //Route::delete('/users/{id}/games', [GameController::class, 'destroyAllGames']); //eliminamos todos los juegos
 //Route::get('/users/winPercentage', [GameController::class, 'allUsersWinPercentage']); //todos los user con sus porcentajes
 //Route::get('/users/{id}/games', [GameController::class, 'getGames']); //mostrar juegos
 //Route::put('/users/{id}', [UserController::class, 'updateName']); //modificar usuario
-//Route::get('/users', [UserController::class, 'index']);
-
-
-/*Route::middleware(middleware:'auth:api')->group(function () {
-});*/
-
-
-
-
+//Route::get('/users', [UserController::class, 'index']);//lista de usuarios
 //Route::get('/users/{id}/winPercentage', [GameController::class, 'winPercentage']); //porcentaje victorias
-//Admmin
- //mostrar usuarios
-
 //Route::get('/users/totalWinPercentage', [GameController::class, 'getTotalWinPercentage']); //Porcentaje total de victorias
-//Route::get('/players/ranking', [GameController::class, 'ranking']); //Porcentaje total de victorias
+
 
 //modo dios
 //Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function(){
